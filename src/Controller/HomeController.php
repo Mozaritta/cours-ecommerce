39a02a1f5a2ef45b1e0beead77553729e9bb2478
Mozaritta <mozaritta@gmail.com>
2021-08-02 19:40:39 +0100

    Update HomeController.php

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -2,18 +2,25 @@
 
 namespace App\Controller;
 
-use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Psr\Log\LoggerInterface;
 
 class HomeController
 {
+    // protected $logger;
+    // public function __construct(LoggerInterface $logger)
+    // {
+    //     $this->logger = $logger;
+    // }
     /**
      * @Route("/home/{nom?World}", name="home", methods={"GET","POST"},
      * host="localhost", schemes={"http","https"})
      */
-    public function home($nom)
+    public function home($nom = "World", LoggerInterface $logger)
     {
+        // $this->logger->error("Mon message de log!");
+        $logger->error("Mon message de log!");
         return new Response("Hello $nom");
     }
 }
