e8876005c356a81ba301e629a38828b98d13d65b
Mozaritta <mozaritta@gmail.com>
2021-08-02 10:41:46 +0100

    First commit

---
+++ src/Controller/TestController.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+
+class TestController
+{
+    public function index()
+    {
+        dump("It's working"); // a beautiful var_dump
+        die();
+    }
+    /**
+     * @Route("/test/{age<\d+>?0}", name="test", method={"GET", "POST"},
+     * host="localhost", schemes={"http","https"})
+     * 
+     */
+    public function test(Request $request, $age)
+    {
+        // $request = Request::createFromGlobals();
+        // $age = $_GET['age'];
+        // dump($request);
+        // $age = $request->query->get('age', 0);
+        // $age = $request->attributes->get('age');
+        // dd(); // == dump and die
+        return new Response("Vous avez $age");
+    }
+}
